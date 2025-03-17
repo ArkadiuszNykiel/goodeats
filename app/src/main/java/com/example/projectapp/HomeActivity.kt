5ee8ad62f4f6afd83ffdc5355ba59217046d34aa
programista <arek.nykiel06@outlook.com>
2025-03-17 16:27:12 +0100

    login + register done

--- app/src/main/java/com/example/projectapp/HomeActivity.kt
+++ app/src/main/java/com/example/projectapp/HomeActivity.kt
@@ -1,12 +1,18 @@
 package com.example.projectapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,18 @@ class HomeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        auth = Firebase.auth
+        val signOutButton: Button = findViewById(R.id.signoutButton)
+        val currentUser = auth.currentUser
+
+
+        signOutButton.setOnClickListener {
+            auth.signOut()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
     }
 }
